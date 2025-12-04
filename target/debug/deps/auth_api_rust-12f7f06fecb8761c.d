bbb9581347de7c2c8e3f2b4fb3fff89e32bb240f
ProPlays2312 <97733495+ProPlays2312@users.noreply.github.com>
2025-12-04 19:07:59 +0530

    Fix: Removed trash from version control

--- target/debug/deps/auth_api_rust-12f7f06fecb8761c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\DELL\RustroverProjects\auth-api-rust\target\debug\deps\auth_api_rust-12f7f06fecb8761c.d: src\main.rs
-
-C:\Users\DELL\RustroverProjects\auth-api-rust\target\debug\deps\libauth_api_rust-12f7f06fecb8761c.rmeta: src\main.rs
-
-src\main.rs:
